72dc532867ef473729b0b915bad75c11dba55ade
jasba24 <johanbuitrago342@gmail.com>
2021-03-09 19:56:33 -0500

    add new file about temporary

---
+++ src/pages/About.js
@@ -0,0 +1,26 @@
+import getData from "../utils/getData"
+import "../styles/main.css"
+
+const About = async () => {
+	const character = await getData()
+	const view = `
+		<div className="Characters-outer">
+		<article class="Characters-card">
+				<img src="${character.image}" alt="${character.name}">
+					<h2>${character.name}</h2>
+				</img>
+			</article>
+			<article class="Characters-card">
+				<h3>Episodes: <span>${character.episode.length}</span></h3>
+				<h3>Status: <span>${character.status}</span></h3>
+				<h3>Species: <span>${character.species}</span></h3>
+				<h3>Gender: <span>${character.gender}</span></h3>
+				<h3>Origin: <span>${character.origin.name}</span></h3>
+				<h3>Last location: ${character.location.name}</h3>
+			</article>
+		</div>
+	`
+	return view
+}
+
+export default About
