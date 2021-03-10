8b0efc90d0db4b9550ff02aa85228bf1e70662c2
jasba24 <johanbuitrago342@gmail.com>
2021-03-10 09:52:24 -0500

    config for clean and optimize the folder dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 
 module.exports = {
@@ -37,5 +40,13 @@ module.exports = {
 				filename: "./index.html",
 			}),
 		new MiniCssExtractPlugin(),
-	]
+		new CleanWebpackPlugin(),
+	],
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new CssMinimizerPlugin(),
+			new TerserPlugin(),
+		]
+	}
 }
